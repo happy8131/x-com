c78ff0793b091c0ed51a5209087a1c9c89938799
happy8131 <lion8131@naver.com>
2024-02-02 18:20:54 +0900

    msw setting, server action, middleware, next-auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { MSWComponent } from "./_component/MCWComponent";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body suppressHydrationWarning={true} className={inter.className}>
+        <MSWComponent />
         {children}
       </body>
     </html>

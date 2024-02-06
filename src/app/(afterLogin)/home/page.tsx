1a251ea643df3bf135da661e99e3da9551aac100
happy8131 <lion8131@naver.com>
2024-02-06 23:15:31 +0900

    react-query client

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,18 +1,34 @@
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
 import Post from "../_component/Post";
 import PostForm from "./_component/PostForm";
 import Tab from "./_component/Tab";
 import TabProvider from "./_component/TabProvider";
 import style from "./home.module.css";
+import PostRecommends from "./_component/PostRecommends";
 
-export default function Home() {
+async function getPostRecommends() {}
+
+export default async function Home() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", "recommends"],
+    queryFn: getPostRecommends,
+  });
+  const dehydratedState = dehydrate(queryClient);
   return (
     <main className={style.main}>
-      <TabProvider>
-        <Tab />
-        <PostForm />
-        <Post />
-        <Post />
-      </TabProvider>
+      <HydrationBoundary state={dehydratedState}>
+        <TabProvider>
+          <Tab />
+          <PostForm />
+          <PostRecommends />
+        </TabProvider>
+      </HydrationBoundary>
     </main>
   );
 }

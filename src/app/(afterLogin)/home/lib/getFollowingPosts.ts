6d12882fa5e4b9a7a198b9befe52b3f458d83fde
happy8131 <lion8131@naver.com>
2024-02-08 23:52:50 +0900

    following/search

---
+++ src/app/(afterLogin)/home/lib/getFollowingPosts.ts
@@ -0,0 +1,20 @@
+type Props = { pageParam?: number };
+export async function getFollowingPosts({ pageParam }: Props) {
+  const res = await fetch(
+    `http://localhost:9090/api/followingPosts?cursor=${pageParam}`,
+    {
+      next: {
+        tags: ["posts", "followings"],
+      },
+    }
+  );
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -8,7 +8,8 @@ import PostForm from "./_component/PostForm";
 import Tab from "./_component/Tab";
 import TabProvider from "./_component/TabProvider";
 import style from "./home.module.css";
-import PostRecommends from "./_component/PostRecommends";
+
+import TabDecider from "./_component/TabDecider";
 
 async function getPostRecommends() {}
 
@@ -26,7 +27,7 @@ export default async function Home() {
         <TabProvider>
           <Tab />
           <PostForm />
-          <PostRecommends />
+          <TabDecider />
         </TabProvider>
       </HydrationBoundary>
     </main>
